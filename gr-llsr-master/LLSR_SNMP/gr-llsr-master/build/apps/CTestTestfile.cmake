51ccf750eee8736ddf2c86d6a7206e31fc134ae8
vincentwang901130 <vincentwang901130@me.com>
2016-11-25 16:04:42 -0500

    add snmp/snmp agent

---
+++ gr-llsr-master/LLSR_SNMP/gr-llsr-master/build/apps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vincent/gr-llsr-master/apps
+# Build directory: /home/vincent/gr-llsr-master/build/apps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ gr-llsr-master/LLSR_SNMP/gr-llsr-master/build/python/CMakeFiles/pygen_python_9a8fe.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pygen_python_9a8fe"
+  "__init__.pyc"
+  "llsr_mac.pyc"
+  "constants.pyc"
+  "__init__.pyo"
+  "llsr_mac.pyo"
+  "constants.pyo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pygen_python_9a8fe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
