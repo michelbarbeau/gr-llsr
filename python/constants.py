4c5d99dc133b4263800e5d99850efe22abc2a8e8
vincentwang901130 <vincentwang901130@me.com>
2017-02-27 21:11:48 -0500

    Code formatted using PEP 8 and fix the broken pipe issues

--- python/constants.py
+++ python/constants.py
@@ -9,33 +9,33 @@
 # Re-using:
 #
 #  File: constants.py
-#  
+#
 #  Copyright 2014 Balint Seeber <balint256@gmail.com>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#------------------------------------------
-#  MGMT INFO AND ERROR CODE 
-#------------------------------------------
+#
+# ------------------------------------------
+#  MGMT INFO AND ERROR CODE
+# ------------------------------------------
 # 0 : SET OPT SUCCESS
 # 1 : OID SET FAIL
 # 2 : DEST NOT FOUND
 # 3 : AUTHENTICATION FAIL
 # 4 : SINK NO NEIGHBOUR NODES DETECTED
-#------------------------------------------
+# ------------------------------------------
 
 # Data/acknowledgement packet  definition
 PKT_PROT_ID = 0
@@ -47,48 +47,48 @@ PROTO_ACK = 4
 
 PKT_CTRL = 4
 
-PKT_MIN = 5 # packet minimum length
-MGMT_MIN = 3 # MGMT packet minimum lenghth
-MGMT_RESP_MIN = 4 # MGMT RESP packet minimum length
-ACK_PKT_LENGTH = 5 # packet length
-MGMT_PKT_LENGTH = 9 # MGMT packet length
-MGMT_RESP_LENGTH = 9 # MGMT resp packet length
+PKT_MIN = 5  # packet minimum length
+MGMT_MIN = 3  # MGMT packet minimum lenghth
+MGMT_RESP_MIN = 4  # MGMT RESP packet minimum length
+ACK_PKT_LENGTH = 5  # packet length
+MGMT_PKT_LENGTH = 9  # MGMT packet length
+MGMT_RESP_LENGTH = 9  # MGMT resp packet length
 # MGMT packet definition
 MGMT_TRACK = 2
-MGMT_ORG=3
+MGMT_ORG = 3
 MGMT_VAL = 4
 MGMT_DEST = 5
 MGMT_OPT = 6
 MGMT_OID = 7
 MGMT_HASH = 8
 # MGMT ACK packet definition
-MGMT_RESP_FLAG=4 # ERROR CODE 1 NORMAL MSG 0
-MGMT_RESP_SRC=5
-MGMT_RESP_TRACK=6
-MGMT_RESP_VAL=7 # ERROR CODE/ NORMAL MSG
-MGMT_RESP_HASH=8
+MGMT_RESP_FLAG = 4  # ERROR CODE 1 NORMAL MSG 0
+MGMT_RESP_SRC = 5
+MGMT_RESP_TRACK = 6
+MGMT_RESP_VAL = 7  # ERROR CODE/ NORMAL MSG
+MGMT_RESP_HASH = 8
 
 # Beacon packet definition
 # PKT_INDEX_PROT_ID = 0
 # PKT_INDEX_SRC = 1
-PKT_HC = 2 # hop count
-PKT_PQ = 3 # path quality
-BEACON_PKT_LENGTH = 4 # packet length
+PKT_HC = 2  # hop count
+PKT_PQ = 3  # path quality
+BEACON_PKT_LENGTH = 4  # packet length
 
 # Protocol ID field
-ARQ_PROTO = 0 # unicast acknowledgement packet
-DATA_PROTO = 1 # unicast user data packet
-BEACON_PROTO = 2 # beacon protocol
-MGMT_PROTO = 3 # management protocol
-MGMT_RESP_PROTO = 4 # management resp protocol
+ARQ_PROTO = 0  # unicast acknowledgement packet
+DATA_PROTO = 1  # unicast user data packet
+BEACON_PROTO = 2  # beacon protocol
+MGMT_PROTO = 3  # management protocol
+MGMT_RESP_PROTO = 4  # management resp protocol
 # sink address
 SINK_ADDR = 0
 # undefined address
 UNDEF_ADDR = -1
 
 # Control field
-NO_ARQ = 0 # ARQ protocol is not applied
-ARQ = 1 # ARQ protocol is applied
+NO_ARQ = 0  # ARQ protocol is not applied
+ARQ = 1  # ARQ protocol is applied
 
 # FSM ARQ states
 CHANNEL_BUSY = 0
--- python/llsrHandler.py
+++ python/llsrHandler.py
@@ -4,22 +4,23 @@ import time
 import sys
 import os
 
+
 class llsrHandler(SocketServer.BaseRequestHandler):
-    mgmttable = None  
+    mgmttable = None
 
     def getColumn(self, idx, name):
         try:
-           value = self.mgmttable.getColumn(idx, name)
-	   return value
+            value = self.mgmttable.getColumn(idx, name)
+            return value
         except Exception as e:
-           sys.stderr.write("Failed to get value: %s" % e)
-           return 'error'
+            sys.stderr.write("Failed to get value: %s" % e)
+            return 'error'
 
     def getTableSize(self):
-	value=self.mgmttable.getTableSize()
-	print ("current table size: %d " % value)
-	return value
-	
+        value = self.mgmttable.getTableSize()
+        print ("current table size: %d " % value)
+        return value
+
     def setColumn(self, idx, name, value):
         self.mgmttable.setColumn(idx, name, value)
 
@@ -31,55 +32,64 @@ class llsrHandler(SocketServer.BaseRequestHandler):
         return self.request.recv(length)
 
     def _sendInt(self, val):
-        self.request.send(struct.pack('I', val))
+        try:
+            self.request.send(struct.pack('I', val))
+        except IOError as e:
+            print('Failed to send int, port shutdown, error: %s' % e)
 
     def _sendStr(self, val):
-        self._sendInt(len(val))
-        self.request.send(val)
+        try:
+            self._sendInt(len(val))
+            self.request.send(val)
+        except IOError as e:
+            print('Failed to send int, port shutdown, error: %s' % e)
 
     def handle(self):
         # self.request is the TCP socket connected to the client
         rtype = self._recvInt()
-	    # get Column
+        # get Column
         if rtype == 0:
-	   idx = self._recvInt()
-	   name = self._recvStr()
-           val = self.getColumn(idx, name)
-           self._sendStr(str(val))
-	    # get tableSize
+            idx = self._recvInt()
+            name = self._recvStr()
+            val = self.getColumn(idx, name)
+            self._sendStr(str(val))
+        # get tableSize
         elif rtype == 1:
-	   val = self.getTableSize()
-	   self._sendInt(val)
-	    # set Column
-	elif rtype == 2:
-	   idx = self._recvInt()
-	   name = self._recvStr()
-           val = self._recvInt()
-           self.setColumn(idx, name, val)
+            val = self.getTableSize()
+            self._sendInt(val)
+        # set Column
+        elif rtype == 2:
+            idx = self._recvInt()
+            name = self._recvStr()
+            val = self._recvInt()
+            self.setColumn(idx, name, val)
         # error type
         else:
-           print("Unrecognized request type %d" % rtype)
-           return
+            print("Unrecognized request type %d" % rtype)
+            return
+
 
 class ManagerServer(SocketServer.UnixStreamServer):
     if os.path.exists("/tmp/udscommunicate"):
-       os.remove("/tmp/udscommunicate")
+        os.remove("/tmp/udscommunicate")
 
-    def __init__(self, tableclass, socketfile = "/tmp/udscommunicate", timeout=0):
-       SocketServer.UnixStreamServer.__init__(self, 
-                                        socketfile,
-                                        llsrHandler)
-       self.socket.settimeout(timeout)
-       self.RequestHandlerClass.mgmttable = tableclass
+    def __init__(self, tableclass, socketfile="/tmp/udscommunicate",
+                 timeout=0):
+        SocketServer.UnixStreamServer.__init__(self,
+                                               socketfile,
+                                               llsrHandler)
+        self.socket.settimeout(timeout)
+        self.RequestHandlerClass.mgmttable = tableclass
 
-#class Test():
+
+# class Test():
 #    def __init__(self, v1):
 #        self.v1 = v1
 
 #    def __str__(self):
 #        return ("{ %s }" % (self.v1))
 
-#def runTest():
+# def runTest():
 #    test = Test("Hello")
 
 #    # Create the server, binding to localhost on port 9999
@@ -96,5 +106,5 @@ class ManagerServer(SocketServer.UnixStreamServer):
 #        print("Test: %s" % test)
 
 
-#if __name__ == "__main__": 
+# if __name__ == "__main__":
 #   runTest()
